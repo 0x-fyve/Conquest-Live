bd9fa46048e6e5a29cffcad3288ac11b5ff070ac
David Odile <davidodile01@gmail,com>
2026-06-26 13:06:19 +0100

    feat: Add Project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+import uuid
 
 # Create your models here.
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="projects", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255,)
+    slug = models.SlugField(max_length=255)
+    description = description = models.TextField(blank=True, default="")
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["owner", "slug"],
+                name="unique_project_slug_per_owner",
+            )
+        ]
+
+    def __str__(self):
+        return self.name
