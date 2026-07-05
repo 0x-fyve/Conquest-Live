a4b0e765d3d84a2a3525920ce7b56a32abcd1fb4
David Odile <davidodile01@gmail.com>
2026-07-05 17:42:51 +0100

    feat: Add Project serializer

---
+++ projects/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Project
+class ProjectSerializer(serializers.ModelSeializer):
+    class Meta:
+        model = Project
+        fields = (
+            "id",
+            "name",
+            "slug",
+            "description",
+            "created_at",
+            "updated_at",
+        )
+        read_only_fields =(
+            "id",
+            "slug",
+            "created_at",
+            "updated_at"
+        )
