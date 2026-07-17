af4584ec0db784f55cccb73ecf38167e8238f54f
David Odile <davidodile01@gmail.com>
2026-07-17 15:54:50 +0100

    feat: Add ScoreEvent get query

--- scoreevents/views.py
+++ scoreevents/views.py
@@ -16,13 +16,15 @@ class ScoreEventViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
 
         event_id = serializer.validated_data.get("event_id")
-        competition = serializer.validated_data.get(competition)
+        competition = serializer.validated_data.get("competition")
         if competition.project.owner != self.request.user:
             return Response(
                 {"error": "You do not own this competition."},
                 status=status.HTTP_403_FORBIDDEN,
             )
         participant = serializer.validated_data.get("participant")
+
+
         points = serializer.validated_data.get("points")
         reason = serializer.validated_data.get("reason", "")
         metadata = serializer.validated_data.get("metadata", None)
@@ -38,3 +40,11 @@ class ScoreEventViewSet(viewsets.ModelViewSet):
         response_serializer = self.get_serializer(score_event)
 
         return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+    
+    def get_queryset(self):
+        return ScoreEvent.objects.filter(
+            competition__project__owner=self.request.user
+        )
+
+        
+        
