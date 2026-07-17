925454d4079242d85040ac681c442fc2e3d56c05
David Odile <davidodile01@gmail.com>
2026-07-17 12:49:11 +0100

    feat: Add Participant viewset

--- participants/views.py
+++ participants/views.py
@@ -3,6 +3,32 @@ from .models import Participant
 from .serializers import ParticipantSerializer
 from .services import ParticipantService
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 
 # Create your views here.
+class ParticipantViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ParticipantSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
+
+        competition = serializer.validated_data.get("competition")
+
+        external_id = serializer.validated_data,get("external_id")
+        display_name = serializer.validated_data.get("display_name")
+        metadata = serializer.validated_data.get("metadata", None)        
+
+        participant = ParticipantService.create_or_update_participant(
+            competition,
+            external_id,
+            display_name,
+            metadata,
+        )
+        response_serializer = self.get_serializer(participant)
+
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+    
+    def get_queryset(self):
+        return Participant.objects.filter(competition__project__owner=self.request.user)
