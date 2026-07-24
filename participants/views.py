c9e10395598952aa69ad91ae5dd8629c4509231c
David Odile <davidodile01@gmail.com>
2026-07-24 11:10:34 +0100

    feat: Add get all parcticipant by competition Id endpoint

--- participants/views.py
+++ participants/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from scoreevents.serializers import ScoreEventSerializer
 
 # Create your views here.
 class ParticipantViewSet(viewsets.ModelViewSet):
